e99cc92d51219a23e613c7795c6b266fbca89494
Civoremo <omerovic_n@hotmail.com>
2019-03-27 21:31:40 -0400

    merge_sort recursion test passed

--- src/recursive_sorting.py
+++ src/recursive_sorting.py
@@ -14,17 +14,29 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # TO-DO
-    print(f'{arr}')
+    # print(f'{arr}')
     middle = len(arr)//2
-    print(f'left: {arr[:middle]} right: {arr[middle:]}')
+    # print(f'left: {arr[:middle]} right: {arr[middle:]}')
     counter = merge(arr[:middle], arr[middle:])
 
-    position = 0
+    position = []
+
+    def rec_counter(index, position, counter):
+        if counter[index] != 0:
+            arr[len(position)] = index
+            counter[index] -= 1
+            position.append(1)
+        else:
+            return 0
+        return rec_counter(index, position, counter)
+
     for i in range(0, len(counter)):
-        while counter[i] != 0:
-            arr[position] = i
-            position += 1
-            counter[i] -= 1
+        rec_counter(i, position, counter)
+        # first pass
+        # while counter[i] != 0:
+        #     arr[position] = i
+        #     position += 1
+        #     counter[i] -= 1
 
     return arr
 
