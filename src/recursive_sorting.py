8651e120352bb865ade2d771ee97837217e45d51
Civoremo <omerovic_n@hotmail.com>
2019-03-28 16:24:12 -0400

    third pass at recursive merge_sort

--- src/recursive_sorting.py
+++ src/recursive_sorting.py
@@ -1,42 +1,56 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA)+1 + len(arrB)+1
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    for i in arrA:
-        merged_arr[i] += 1
-    for i in arrB:
-        merged_arr[i] += 1
+    # second pass
+    # for i in arrA:
+    #     merged_arr[i] += 1
+    # for i in arrB:
+    #     merged_arr[i] += 1
 
+    # third pass
+    first = 0
+    second = 0
+    for index in range(0, elements):
+        if first >= len(arrA):
+            merged_arr[index] = arrB[second]
+            second += 1
+        elif second >= len(arrB):
+            merged_arr[index] = arrA[first]
+            first += 1
+        elif arrA[first] < arrB[second]:
+            merged_arr[index] = arrA[first]
+            first += 1
+        else:
+            merged_arr[index] = arrB[second]
+            second += 1
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # TO-DO
-    # print(f'{arr}')
-    middle = len(arr)//2
-    # print(f'left: {arr[:middle]} right: {arr[middle:]}')
-    counter = merge(arr[:middle], arr[middle:])
+    # second pass
+    # middle = len(arr)//2
+    # counter = merge(arr[:middle], arr[middle:])
+    # position = []
+    # def rec_counter(index, position, counter):
+    #     if counter[index] != 0:
+    #         arr[len(position)] = index
+    #         counter[index] -= 1
+    #         position.append(1)
+    #     else:
+    #         return 0
+    #     return rec_counter(index, position, counter)
+    # for i in range(0, len(counter)):
+    #     rec_counter(i, position, counter)
 
-    position = []
-
-    def rec_counter(index, position, counter):
-        if counter[index] != 0:
-            arr[len(position)] = index
-            counter[index] -= 1
-            position.append(1)
-        else:
-            return 0
-        return rec_counter(index, position, counter)
-
-    for i in range(0, len(counter)):
-        rec_counter(i, position, counter)
-        # first pass
-        # while counter[i] != 0:
-        #     arr[position] = i
-        #     position += 1
-        #     counter[i] -= 1
+    # third pass
+    if len(arr) > 1:
+        left = merge_sort(arr[0: len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
 
     return arr
 
